abb47575ecc1a7e5641e977febf5997b398066c4
Saju Sasidharan <Saju.Sasidharan-Cybage@domo.com>
2019-01-22 19:50:01 +0530

    Initial Commits

--- kingcobra.py
+++ kingcobra.py
@@ -1,16 +1,24 @@
 #
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
 import time
+
+#Default wait time
+defaultWait = 10
 
 #Default path for chromedriver is '/usr/local/bin/';
 driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 10);
 
 driver.get('https://www.google.com/webhp?gws_rd=ssl');
-time.sleep(5)
+
+search_box = wait.until(expected_conditions.presence_of_element_located((By.NAME, 'q')));
+print(search_box)
 
 # Web Element API:
 # https://seleniumhq.github.io/selenium/docs/api/py/webdriver_remote/selenium.webdriver.remote.webelement.html
-search_box = driver.find_element_by_name('q')
 
 search_box.send_keys('Saju Sasidharan')
 search_box.submit()
